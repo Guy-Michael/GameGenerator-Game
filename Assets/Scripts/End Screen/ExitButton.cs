6281958999c8c61ab08e1563025d26bfc29e0080
Guy Rahamim <guy.danino5@gmail.com>
2023-07-19 20:18:27 +0300

    add end screen exit, restart buttons

---
+++ Assets/Scripts/End Screen/ExitButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitButton : MonoBehaviour
+{
+    void Start()
+    {
+        Button b = GetComponent<Button>();
+        b.onClick.AddListener(Application.Quit);
+    }
+}
--- Assets/Scripts/Game Manager/SceneTransitionManager.cs
+++ Assets/Scripts/Game Manager/SceneTransitionManager.cs
@@ -11,4 +11,9 @@ public static class SceneTransitionManager
         int index = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(index + 1);
     }
+
+    public static void MoveToGameScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
 }
