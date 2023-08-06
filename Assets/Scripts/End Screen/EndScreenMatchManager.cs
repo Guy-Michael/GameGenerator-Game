4969387ed9706ec0f911450d79d9228a655cb8f7
Guy Rahamim <guy.danino5@gmail.com>
2023-08-06 23:53:51 +0300

    add new feedback screen layout

--- Assets/Scripts/End Screen/EndScreenMatchManager.cs
+++ Assets/Scripts/End Screen/EndScreenMatchManager.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,13 +7,24 @@ public class EndScreenMatchManager : MonoBehaviour
 {
     [SerializeField] EndScreenMatch matchPrefab;
     [SerializeField] Transform matchContainer;
-
-    public void Init(List<(Sprite sprite, string caption, bool isCorrect)> matchList)
+    [SerializeField] Player player;
+    
+    void Start()
     {
-        foreach((Sprite sprite, string caption, bool isCorrect) item in matchList)
+        var matchList = AnalyticsManager.analytics[player].moves;
+        var correctMatches = matchList.Where(match => match.isCorrect);
+        var wrongMatches = matchList.Where(match => !match.isCorrect);
+
+        foreach((Sprite sprite, string caption, bool isCorrect) in correctMatches)
         {
             EndScreenMatch match = Instantiate(matchPrefab, matchContainer);
-            match.Init(item.sprite, item.caption, item.isCorrect);
+            match.Init(sprite, caption, isCorrect);
+        }
+        
+        foreach((Sprite sprite, string caption, bool isCorrect) in wrongMatches)
+        {
+            EndScreenMatch match = Instantiate(matchPrefab, matchContainer);
+            match.Init(sprite, caption, isCorrect);
         }
     }
 }
