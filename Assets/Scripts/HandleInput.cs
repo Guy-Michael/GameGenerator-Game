2d03e2f0bdb2516908738ea142adae3d41d0fe28
Guy Rahamim <guy.danino5@gmail.com>
2023-08-18 00:53:22 +0300

    change button, logo sprites on main menu

--- Assets/Scripts/HandleInput.cs
+++ Assets/Scripts/HandleInput.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,16 +9,27 @@ public class HandleInput : MonoBehaviour
 {
     TMP_InputField text;
     Button button;
+    [SerializeField] Color disabledColor;
+    [SerializeField] Color enabledColor;
     void Start()
     {
         text = transform.parent.Find("Code Input").GetComponent<TMP_InputField>();
+        text.onValueChanged.AddListener((val) => changeButtonColor());
         button = GetComponent<Button>();
         GetComponent<Button>().onClick.AddListener(OnStartButtonClicked);
+        button.interactable = false;
     }
 
-    void Update()
+    void changeButtonColor()
     {
         button.interactable = text.text.Length > 0;
+        var colors = button.colors;
+        colors.normalColor = text.text.Length > 0 ? enabledColor : disabledColor;
+        colors.normalColor = enabledColor;
+        colors.highlightedColor = enabledColor * 1.7f;
+        colors.pressedColor = enabledColor * 1.3f;
+        colors.selectedColor = colors.highlightedColor;
+        button.colors = colors;
     }
     
     void OnStartButtonClicked()
